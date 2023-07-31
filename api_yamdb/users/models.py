ddcfc652f649fc45811cd66cd5b31ca005144503
Максим Уколов <m.ukolov@gmail.com>
2023-07-31 23:45:10 +0300

    Update

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    USER_ROLES = [
+        (USER, 'User'),
+        (MODERATOR, 'Moderator'),
+        (ADMIN, 'Admin'),
+    ]
+
+    email = models.EmailField(
+        'email',
+        max_length=254,
+        unique=True
+    )
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        'Роль',
+        max_length=9,
+        choices=USER_ROLES,
+        default=USER,
+    )
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    def __str__(self):
+        return self.username
