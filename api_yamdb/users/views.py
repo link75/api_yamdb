1003fe30d4143b766386d4e93b6f417a9112f2e9
Максим Уколов <m.ukolov@gmail.com>
2023-08-01 22:41:44 +0300

    Update

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -12,6 +12,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
+
 from .models import User
 from .permissions import IsAdmin
 from .serializers import (RegistrationSerializer, TokenSerializer,
