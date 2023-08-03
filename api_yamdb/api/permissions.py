2391ee35bec1f38373eef501d4faf6449f8c5a35
Ростислав Тимофеев <timofeevrd@gmail.com>
2023-08-03 05:46:12 +0300

    Bugfixing Titles

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,4 +17,4 @@ class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
-                or request.user.is_authenticated and request.user.is_admin)
+                or (request.user.is_authenticated and request.user.is_admin))
